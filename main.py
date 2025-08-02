8f99e0a813dc8759c7ee126f014516712671d39f
chocapiklover <a.harmuth33@gmail.com>
2025-08-02 13:05:25 +0200

    added the 'verbose' flag as an input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 load_dotenv()
@@ -13,16 +14,33 @@ def main():
     if len(sys.argv) < 2:
         print("enter a prompt")
         sys.exit(1)
+    
+    
 
     user_prompt = sys.argv[1]
+    messages = [
+        types.Content(
+            role='user',
+            parts=[types.Part(text=user_prompt)],
+        )
+    ]
+
+    # system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=user_prompt
-
+        model='gemini-2.0-flash-001',
+        contents=messages
+        # config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
+
+    if len(sys.argv) >= 3 and sys.argv[2] == '--verbose':
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    
 
 
 if __name__ == "__main__":
