5c0f46980a4a416a5f4ef648b6c188b8326c361a
chocapiklover <a.harmuth33@gmail.com>
2025-08-06 23:50:30 +0200

    updated and created new schemas for all the available functions

--- functions/schemas.py
+++ functions/schemas.py
@@ -13,3 +13,57 @@ schema_get_files_info = types.FunctionDeclaration(
         },
     ),
 )
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Read the contents within the file.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to read, relative to the working directory."
+            )
+        },
+        required=["file_path"]
+    )
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Execute a Python file with optional arguments, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the Python file to execute, relative to the working directory."
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                description="Optional list of string arguments to pass to the Python script.",
+                items=types.Schema(type=types.Type.STRING)
+            )
+        },
+        required=["file_path"]
+    )
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Write content to a file in the working directory. If the file or path does not exist, it will be created.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write, relative to the working directory."
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write into the file."
+            )
+        },
+        required=["file_path", "content"]
+    )
+)
--- functions/tools.py
+++ functions/tools.py
@@ -1,8 +1,11 @@
 from google.genai import types
-from .schemas import schema_get_files_info
+from .schemas import schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file
 
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
     ]
 )
